580e777fb0e95d7ed18ea75643d7e897cd007cdd
Brijesh Kasaudhan <brijeshkausdhan@Brijeshs-MacBook-Air.local>
2025-10-12 15:18:23 +0530

    Intitiall commitment

---
+++ Evenodd.java
@@ -0,0 +1,12 @@
+public class Evenodd {
+    public static void main(String[] args) {
+        int number = 7;
+        if (number % 2 == 0) {
+            System.out.println("Even number");            
+        }
+        else{
+            System.out.println("odd number");
+        }
+    }
+    
+}
---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 2;
+        int b= 28;
+        int sum = a+b;
+        System.out.println("Sum is :",sum);
+    }
+}
